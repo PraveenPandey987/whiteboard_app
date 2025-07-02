9c3eaec235145a2f44467fa017c0a624add354f8
praveen <pp7007144435@gmail.com>
2025-07-02 11:47:39 +0530

    updated

--- vite.config.js
+++ vite.config.js
@@ -1,32 +1,7 @@
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  optimizeDeps: {
-    include: [
-      'roughjs',
-      'classnames',
-      'perfect-freehand',
-      'react-icons',
-      'react-icons/lu',
-      'react-icons/fa'
-    ]
-  },
-  
-  build: {
-    commonjsOptions: {
-      transformMixedEsModules: true,
-      include: [/node_modules/]
-    },
-  },
-  resolve: {
-    alias: {
-      '@': '/src'
-    }
-  },
-  server: {
-    port: 3000,
-    open: true
-  }
 })
