30e99af0b0fe6e8605c6fd0ccc892c4de3da23fb
praveen <pp7007144435@gmail.com>
2025-07-02 10:39:09 +0530

    updated5

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,35 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-
 export default defineConfig({
   plugins: [react()],
   optimizeDeps: {
-    include: ['roughjs','classnames']
-
+    include: [
+      'roughjs',
+      'classnames', 
+      'perfect-freehand',
+      'react-icons'
+    ]
   },
   build: {
     commonjsOptions: {
-      include: [/roughjs/, /node_modules/, /classnames/]
+      transformMixedEsModules: true,
+      include: [/node_modules/]
+    },
+    rollupOptions: {
+      external: ['roughjs', 'classnames', 'perfect-freehand'],
+      output: {
+        globals: {
+          'roughjs': 'rough',
+          'classnames': 'classNames',
+          'perfect-freehand': 'perfectFreehand'
+        }
+      }
+    }
+  },
+  resolve: {
+    alias: {
+      // Add any path aliases if needed
     }
   }
 })
